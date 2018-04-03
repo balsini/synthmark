813889e5cd886b618d438f5d4dd2f493049756cf
Alessio Balsini <alessio.balsini@arm.com>
2018-04-03 15:33:48 +0100

    Custom CPU manager class for DL
    The new class uses SCHED_DEADLINE to drive the user callback's runtime
    in such a way that the CPU frequency can be adjusted according to the
    application workload.

--- source/tools/VirtualAudioSink.h
+++ source/tools/VirtualAudioSink.h
@@ -179,6 +179,25 @@ public:
         setSchedFifoUsed(false);
         if (callback != NULL) {
             if (mUseRealThread) {
+#ifdef SYNTHMARK_USE_CUSTOM_CPU_MANAGER
+                double initial_bw = 0.94;
+
+                int err = dynamic_cast<CustomHostCpuManager *>(HostCpuManager::getInstance())->updateDeadlineParams(
+                            mNanosPerBurst * initial_bw,
+                            mNanosPerBurst,
+                            mNanosPerBurst);
+                if (err == 0) {
+                    printf("requestDLBandwidth(%lf)\n", initial_bw);
+                    fflush(stdout);
+                } else {
+                    printf("Error setting SCHED_DEADLINE\n");
+                    exit(-1);
+                }
+                if (getRequestedCpu() != SYNTHMARK_CPU_UNSPECIFIED) {
+                    printf("Error setting affinity for SCHED_DEADLINE task\n");
+                    exit(-1);
+                }
+#else
                 int err = mThread->promote(mThreadPriority);
                 if (err == 0) {
                     setSchedFifoUsed(true);
@@ -191,6 +210,7 @@ public:
                         setActualCpu(getRequestedCpu());
                     }
                 }
+#endif
             }
 
             // Write in a loop until the callback says we are done.
